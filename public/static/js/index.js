20d26af48fcc481fa3df91579965e7a54ea88c13
GiantMing <971334483@qq.com>
2016-11-08 22:27:17 +0800

    改接口

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -26,15 +26,20 @@ var $thumb = $('.thumb');
 $thumb.on('click', function(e) {
     var $this = $(this);
 
-    var college = $this.attr('data-college');
+    var academy = $this.attr('data-academy');
     var id = $this.attr('data-id');
     
-    var areYouConfirm = confirm("确定将你宝贵的一票投给Ta吗?");
+    var areYouConfirm = confirm("确定将你宝贵的一票投给Ta们吗?");
+    
     if(!areYouConfirm) return;
 
-    $.post('/path/to/file', {college: college, id: id}, function(data, textStatus, xhr) {
+    $.post('/path/to/file', {academy: academy, id: id}, function(data, textStatus, xhr) {
         var status = data.status;
-        
+        if(status === 200) {
+            location.reload();
+        } else {
+
+        }
     });
 
 });
