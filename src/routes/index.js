e98de9d58d99efec5383869d77e18e48ce620ecc
GiantMing <971334483@qq.com>
2016-11-18 11:39:25 +0800

    静态资源路径问题

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,23 +7,14 @@ import adminCtrl from '../controllers/adminLoginCtrl'
 import changeStatusCtrl from '../controllers/changeStatusCtrl'
 import typeStatusCtrl from '../controllers/typeStatusCtrl'
 
-const router = new Router({
-  prefix: '/vote_drx'
-})
+const router = Router()
 
-router.get('/index', indexCtrl)
-router.post('/vote', voteCtrl)
-router.post('/adminLogin', adminCtrl)
-router.post('/changeStatus', changeStatusCtrl)
+router.get('/vote_drx/index', indexCtrl)
+router.post('/vote_drx/vote', voteCtrl)
+router.post('/vote_drx/adminLogin', adminCtrl)
+router.post('/vote_drx/changeStatus', changeStatusCtrl)
 
-router.get('/typeStatus', typeStatusCtrl)
-
-// router.get('/test', async (ctx) => {
-//     await ctx.render('index')
-// })
-// router.get('/admin', async (ctx) => {
-//     await ctx.render('admin')
-// })
+router.get('/vote_drx/typeStatus', typeStatusCtrl)
 
 
 
