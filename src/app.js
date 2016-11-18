2f9fc6e6fbd9430505c48c57e011780cd3e634d3
GiantMing <971334483@qq.com>
2016-11-18 11:28:39 +0800

    静态资源路径问题

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ app.use(convert(koaSession(app)))
 
 // static
 app.use(convert(koaStatic(path.join(__dirname, '../public'), {
-  pathPrefix: '/vote_drx'
+  pathPrefix: '/vote_drx/'
 })))
 
 // views
